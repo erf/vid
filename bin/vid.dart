e6ee8731fa405e4779f63b05d8d04661b86a5dd2
Erlend Lind Madsen <erlendf80@gmail.com>
2023-01-02 18:46:39 +0100

    init dart console project

---
+++ bin/vid.dart
@@ -0,0 +1,5 @@
+import 'package:vid/vid.dart' as vid;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vid.calculate()}!');
+}
---
+++ test/vid_test.dart
@@ -0,0 +1,8 @@
+import 'package:vid/vid.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
