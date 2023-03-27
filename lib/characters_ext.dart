1e2ea8047885f00c8233571ddd39548d220fb646
Erlend Lind Madsen <erlendf80@gmail.com>
2023-03-27 02:47:28 +0200

    CharactersExt: fix substring

--- lib/characters_ext.dart
+++ lib/characters_ext.dart
@@ -2,7 +2,7 @@ import 'package:characters/characters.dart';
 
 extension CharactersExt on Characters {
   Characters substring(int start, [int? end]) {
-    return skip(start).take(end ?? length);
+    return skip(start).take((end ?? length) - start);
   }
 
   Characters replaceRange(int start, int? end, Characters replacement) {
