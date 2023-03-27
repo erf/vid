ecca089233dc451235d134e60825ca58d2682dd9
Erlend Lind Madsen <erlendf80@gmail.com>
2023-03-27 11:47:43 +0200

    vt100: specify String type

--- lib/vt100.dart
+++ lib/vt100.dart
@@ -3,23 +3,23 @@ class VT100 {
   VT100._();
 
   // move cursor to x,y
-  static cursorPosition({required int x, required int y}) => '\x1b[$y;${x}H';
+  static String cursorPosition({required int x, required int y}) => '\x1b[$y;${x}H';
 
   // cursor visibility
-  static cursorVisible(bool visible) => visible ? '\x1b[?25h' : '\x1b[?25l';
+  static String cursorVisible(bool visible) => visible ? '\x1b[?25h' : '\x1b[?25l';
 
   // home and erase down
-  static const erase = '\x1b[H\x1b[J';
+  static const String erase = '\x1b[H\x1b[J';
 
   // set foreground color
-  static foreground(int color) => '\x1b[38;5;${color}m';
+  static String foreground(int color) => '\x1b[38;5;${color}m';
 
   // set background color
-  static background(int color) => '\x1b[48;5;${color}m';
+  static String background(int color) => '\x1b[48;5;${color}m';
 
   // set invert
-  static invert(invert) => invert ? '\x1b[7m' : '\x1b[27m';
+  static String invert(invert) => invert ? '\x1b[7m' : '\x1b[27m';
 
   // reset font and background color
-  static const reset = '\x1b[0m';
+  static const String reset = '\x1b[0m';
 }
