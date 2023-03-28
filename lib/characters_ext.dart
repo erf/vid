a97fea9ae9bd1dbcbaee20442a927241f9e33e03
Erlend Lind Madsen <erlendf80@gmail.com>
2023-03-28 22:02:25 +0200

    characters_ext: indent comments

--- lib/characters_ext.dart
+++ lib/characters_ext.dart
@@ -9,12 +9,12 @@ extension CharactersExt on Characters {
     return substring(0, start) + replacement + substring(end ?? length);
   }
 
-// get the cursor position for the rendered line, taking into account multi-characters
+  // get the cursor position for the rendered line, taking into account multi-characters
   int getCursorPosition(int char) {
     return take(char).fold(0, (prev, curr) => prev + (curr.length > 1 ? 2 : 1));
   }
 
-// get the rendered line width, taking into account multi-characters
+  // get the rendered line width, taking into account multi-characters
   int getRenderedLength() {
     return fold(0, (prev, curr) => prev + (curr.length > 1 ? 2 : 1));
   }
