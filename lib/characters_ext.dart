b6ada23d62e5bef01b9c4ed38dadb1b99eb6775f
Erlend Lind Madsen <erlendf80@gmail.com>
2023-03-28 19:19:07 +0200

    characters_ext: add getCursorPosition and getRendereredLength

--- lib/characters_ext.dart
+++ lib/characters_ext.dart
@@ -8,4 +8,14 @@ extension CharactersExt on Characters {
   Characters replaceRange(int start, int? end, Characters replacement) {
     return substring(0, start) + replacement + substring(end ?? length);
   }
+
+// get the cursor position for the rendered line, taking into account multi-characters
+  int getCursorPosition(int char) {
+    return take(char).fold(0, (prev, curr) => prev + (curr.length > 1 ? 2 : 1));
+  }
+
+// get the rendered line width, taking into account multi-characters
+  int getRenderedLength() {
+    return fold(0, (prev, curr) => prev + (curr.length > 1 ? 2 : 1));
+  }
 }
