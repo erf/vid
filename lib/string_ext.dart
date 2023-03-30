9c594b9428e15e643bd1d10fb2f8539a82568dd3
Erlend Lind Madsen <erlendf80@gmail.com>
2023-03-31 01:37:54 +0200

    make stringWidth optional

--- lib/string_ext.dart
+++ lib/string_ext.dart
@@ -1,7 +1,13 @@
 import 'package:string_width/string_width.dart';
 
+import 'config.dart';
+
 extension StringExt on String {
   int get renderWidth {
-    return stringWidth(this);
+    if (Config.useLatestUnicodeVersion) {
+      return stringWidth(this);
+    } else {
+      return runes.first > 0x10000 ? 2 : 1;
+    }
   }
 }
