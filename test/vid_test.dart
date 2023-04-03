9a1b6354275df4023ed764f4072153b42d737a10
Erlend Lind Madsen <erlendf80@gmail.com>
2023-04-03 10:56:56 +0200

    test motionCharPrev

--- test/vid_test.dart
+++ test/vid_test.dart
@@ -16,4 +16,16 @@ void main() {
     expect(motionCharNext(f, Position(x: 0, y: 1)), Position(x: 1, y: 1));
     expect(motionCharNext(f, Position(x: 2, y: 1)), Position(x: 2, y: 1));
   });
+
+  test('motionCharPrev', () {
+    final f = FileBuffer();
+    f.lines = [
+      'abc'.ch,
+      'def'.ch,
+    ];
+    expect(motionCharPrev(f, Position(x: 0, y: 0)), Position(x: 0, y: 0));
+    expect(motionCharPrev(f, Position(x: 2, y: 0)), Position(x: 1, y: 0));
+    expect(motionCharPrev(f, Position(x: 0, y: 1)), Position(x: 0, y: 1));
+    expect(motionCharPrev(f, Position(x: 2, y: 1)), Position(x: 1, y: 1));
+  });
 }
