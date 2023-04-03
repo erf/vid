c339210d46916390dfe4a110b4f32fd04f0434fa
Erlend Lind Madsen <erlendf80@gmail.com>
2023-04-03 10:58:16 +0200

    test: motionCharUp

--- test/vid_test.dart
+++ test/vid_test.dart
@@ -28,4 +28,16 @@ void main() {
     expect(motionCharPrev(f, Position(x: 0, y: 1)), Position(x: 0, y: 1));
     expect(motionCharPrev(f, Position(x: 2, y: 1)), Position(x: 1, y: 1));
   });
+
+  test('motionCharUp', () {
+    final f = FileBuffer();
+    f.lines = [
+      'abc'.ch,
+      'def'.ch,
+    ];
+    expect(motionCharUp(f, Position(x: 0, y: 0)), Position(x: 0, y: 0));
+    expect(motionCharUp(f, Position(x: 2, y: 0)), Position(x: 2, y: 0));
+    expect(motionCharUp(f, Position(x: 0, y: 1)), Position(x: 0, y: 0));
+    expect(motionCharUp(f, Position(x: 2, y: 1)), Position(x: 2, y: 0));
+  });
 }
