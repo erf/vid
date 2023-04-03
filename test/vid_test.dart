4cdc707b6ac132cf6d0563f5c29297a22219dd31
Erlend Lind Madsen <erlendf80@gmail.com>
2023-04-03 10:49:16 +0200

    test: motionCharNext

--- test/vid_test.dart
+++ test/vid_test.dart
@@ -1,6 +1,22 @@
+import 'package:vid/actions_motion.dart';
 import 'package:vid/editor.dart';
 import 'package:test/test.dart';
+import 'package:vid/file_buffer.dart';
+import 'package:vid/position.dart';
+import 'package:vid/string_ext.dart';
 
 void main() {
-  test('calculate', () {});
+  test('motionCharNext', () {
+    final f = FileBuffer();
+    f.lines = [
+      'abc'.ch,
+      'def'.ch,
+    ];
+    expect(motionCharNext(f, Position(x: 0, y: 0)), Position(x: 1, y: 0));
+    expect(motionCharNext(f, Position(x: 1, y: 0)), Position(x: 2, y: 0));
+    expect(motionCharNext(f, Position(x: 2, y: 0)), Position(x: 2, y: 0));
+    expect(motionCharNext(f, Position(x: 0, y: 1)), Position(x: 1, y: 1));
+    expect(motionCharNext(f, Position(x: 1, y: 1)), Position(x: 2, y: 1));
+    expect(motionCharNext(f, Position(x: 2, y: 1)), Position(x: 2, y: 1));
+  });
 }
