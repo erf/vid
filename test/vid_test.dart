f40e2b34f49ad187bd9f8c03aa2df0db148da279
Erlend Lind Madsen <erlendf80@gmail.com>
2023-04-03 10:54:25 +0200

    test: remove some superfluos expects

--- test/vid_test.dart
+++ test/vid_test.dart
@@ -1,6 +1,5 @@
-import 'package:vid/actions_motion.dart';
-import 'package:vid/editor.dart';
 import 'package:test/test.dart';
+import 'package:vid/actions_motion.dart';
 import 'package:vid/file_buffer.dart';
 import 'package:vid/position.dart';
 import 'package:vid/string_ext.dart';
@@ -13,10 +12,8 @@ void main() {
       'def'.ch,
     ];
     expect(motionCharNext(f, Position(x: 0, y: 0)), Position(x: 1, y: 0));
-    expect(motionCharNext(f, Position(x: 1, y: 0)), Position(x: 2, y: 0));
     expect(motionCharNext(f, Position(x: 2, y: 0)), Position(x: 2, y: 0));
     expect(motionCharNext(f, Position(x: 0, y: 1)), Position(x: 1, y: 1));
-    expect(motionCharNext(f, Position(x: 1, y: 1)), Position(x: 2, y: 1));
     expect(motionCharNext(f, Position(x: 2, y: 1)), Position(x: 2, y: 1));
   });
 }
