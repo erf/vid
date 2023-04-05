33de4cca042b32809ec99012058559edfd3cf94c
Erlend Lind Madsen <erlendf80@gmail.com>
2023-04-05 22:15:33 +0200

    renderWidth: check for VS15 and VS16 to determine len

--- lib/string_ext.dart
+++ lib/string_ext.dart
@@ -1,13 +1,28 @@
 import 'package:characters/characters.dart';
 
 extension StringExt on String {
-  // Naive implementation to determine the rendered width of a character.
-  // return 1 for characters in the Basic Multilingual Plane (BMP)
-  // return 2 for characters outside the BMP
-  // https://en.wikipedia.org/wiki/Plane_(Unicode)#Basic_Multilingual_Plane
+  // Try to determine the rendered width of a string
   int get renderWidth {
+    // if the string is empty, return 0
+    if (isEmpty) {
+      return 0;
+    }
+
+    // Check if codeUnits contains a Variation Selector (VS) of type 16 (emoji) or 15 (text)
+    // https://en.wikipedia.org/wiki/Variation_Selectors_(Unicode_block)
+    // http://www.unicode.org/reports/tr51/#def_emoji_presentation
+    const int vs15 = 0xFE0E; // text
+    const int vs16 = 0xFE0F; // emoji
+    for (final codeUnit in codeUnits) {
+      if (codeUnit == vs15) return 1;
+      if (codeUnit == vs16) return 2;
+    }
+
+    // Return 1 if the first character is in the Basic Multilingual Plane (BMP) else return 2
+    // https://en.wikipedia.org/wiki/Plane_(Unicode)#Basic_Multilingual_Plane
     return runes.first >= 0x10000 ? 2 : 1;
   }
 
+  // Shorthand for Characters(this)
   Characters get ch => Characters(this);
 }
---
+++ test/string_test.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+import 'package:test/test.dart';
+import 'package:vid/string_ext.dart';
+
+void info(String str) {
+  //print(str);
+  //print(str.length);
+  //print(str.codeUnits.length);
+  //print(str.codeUnits);
+  //print(str.runes.map((e) => toHex(e)).join(' '));
+  //print(str.renderWidth);
+  print('$str ${str.renderWidth}');
+}
+
+String toHex(int value) {
+  return '0x${value.toRadixString(16).toUpperCase()}';
+}
+
+void main() {
+  test('Render width', () {
+    expect(''.renderWidth, 0);
+    expect('a'.renderWidth, 1);
+    expect('▪︎'.renderWidth, 1);
+    expect('▪️'.renderWidth, 2);
+    expect('❤️'.renderWidth, 2);
+    expect('💕'.renderWidth, 2);
+    expect('👩‍👩‍👦‍👦'.renderWidth, 2);
+  });
+}
