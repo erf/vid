d0892cf7d198a3b27b5d4a41ec80f6d1bd991083
Erlend Lind Madsen <erlendf80@gmail.com>
2023-04-08 14:06:17 +0200

    test: more string width text

--- test/string_test.dart
+++ test/string_test.dart
@@ -1,24 +1,19 @@
-import 'dart:math';
-
 import 'package:test/test.dart';
 import 'package:vid/string_ext.dart';
+import 'package:vid/int_ext.dart';
 
 void info(String str) {
-  //print(str);
-  //print(str.length);
-  //print(str.codeUnits.length);
+  print(str);
+  print(str.length);
+  print(str.codeUnits.length);
   print(str.codeUnits);
-  print(str.runes.map((e) => toHex(e)).join(' '));
-  //print(str.renderWidth);
+  print(str.runes.map((e) => e.hex).join(' '));
+  print(str.renderWidth);
   print('$str ${str.renderWidth}');
 }
 
-String toHex(int value) {
-  return '0x${value.toRadixString(16).toUpperCase()}';
-}
-
 void main() {
-  test('Render width', () {
+  test('Emoji render width', () {
     expect(''.renderWidth, 0);
     expect('a'.renderWidth, 1);
     expect('▪︎'.renderWidth, 1);
@@ -40,8 +35,6 @@ void main() {
     expect('\u23F1'.renderWidth, 1);
     expect('\u23F2'.renderWidth, 1);
     expect('\u23F8'.renderWidth, 1);
-    expect('\u23F9'.renderWidth, 1);
-    expect('\u23FA'.renderWidth, 1);
   });
 
   test('Default presentation emoji', () {
@@ -53,5 +46,14 @@ void main() {
     expect('\u23EC'.renderWidth, 2);
     expect('\u23F0'.renderWidth, 2);
     expect('\u23F3'.renderWidth, 2);
+  });
+
+  test('Emoji vs Text types with variations', () {
+    expect('⌛'.renderWidth, 2, reason: '⌛ emoji');
+    expect('⌛︎'.renderWidth, 1, reason: '⌛ emoji + VS15');
+    expect('⌛️'.renderWidth, 2, reason: '⌛ emoji + VS16');
+    expect('⌨'.renderWidth, 1, reason: '⌨ text');
+    expect('⌨︎'.renderWidth, 1, reason: '⌨︎ text + VS15');
+    expect('⌨️'.renderWidth, 2, reason: '⌨️ text + VS16');
   });
 }
