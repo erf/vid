c41486803179a349158a7b055bb9a4aaedb305a2
Erlend Lind Madsen <erlendf80@gmail.com>
2023-04-08 13:51:04 +0200

    emoji tool, support parsing 1.0 emojis

--- tools/parse_emoji_data.dart
+++ tools/parse_emoji_data.dart
@@ -2,6 +2,12 @@ import 'dart:io';
 
 // This script parses the emoji-data.txt file from the Unicode Consortium
 // and outputs a list of code points that are emoji.
+//
+// We support all emoji in the Emoji 1.0 spec, plus all emoji in the latest spec.
+//
+// The emoji-data.txt files can be downloaded from:
+// https://unicode.org/Public/UCD/latest/ucd/emoji/emoji-data.txt
+// https://unicode.org/Public/emoji/1.0/emoji-data.txt
 int main(List<String> args) {
   final path = args.first;
   if (Uri.tryParse(path) == null) {
@@ -25,17 +31,27 @@ int main(List<String> args) {
       continue;
     }
     final name = parts[1].trim();
-    if (!name.contains('Emoji_Presentation')) {
+    if (!name.contains('Emoji_Presentation') && !name.contains('emoji')) {
       continue;
     }
-    final codePoints = parts[0].trim().split('..');
-    final codePoint = int.parse(codePoints.first, radix: 16);
-    emojiCodePoints.add(codePoint);
-    if (codePoints.length > 1) {
-      final endCodePoint = int.parse(codePoints.last, radix: 16);
-      for (var i = codePoint + 1; i <= endCodePoint; i++) {
-        emojiCodePoints.add(i);
+    final value = parts[0].trim();
+
+    // Unicode 15.0 has a range of code points for emoji
+    if (value.contains('..')) {
+      final codePoints = value.split('..');
+      final codePoint = int.parse(codePoints.first, radix: 16);
+      emojiCodePoints.add(codePoint);
+      if (codePoints.length > 1) {
+        final endCodePoint = int.parse(codePoints.last, radix: 16);
+        for (var i = codePoint + 1; i <= endCodePoint; i++) {
+          emojiCodePoints.add(i);
+        }
       }
+    } else {
+      // Unicode 1.0 has a single code point for emoji
+      final codePoints = value.split(' ');
+      final codePoint = int.parse(codePoints.first, radix: 16);
+      emojiCodePoints.add(codePoint);
     }
   }
   // write out the code points as a list of ints in hex
