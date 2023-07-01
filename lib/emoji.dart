10423c1155f1dfbb25719811eca78fbb2af025b5
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-02 00:53:27 +0200

    emojis: cleanup

--- lib/emoji.dart
+++ lib/emoji.dart
@@ -1,31 +1,26 @@
-import 'package:vid/emojis_2.dart';
-
 import 'emojis_1.dart';
 import 'emojis_15.dart';
 
 class Emoji {
-  // Check if a character is an emoji
   static bool isEmoji(String str) {
     // Check if codeUnits contains a Variation Selector (VS) of type 16 (emoji) or 15 (text)
     // https://en.wikipedia.org/wiki/Variation_Selectors_(Unicode_block)
     // http://www.unicode.org/reports/tr51/#def_emoji_presentation
     const int vs15 = 0xFE0E; // text
+    const int vs16 = 0xFE0F; // emoji
     if (str.codeUnits.contains(vs15)) {
-      //print('vs15: $this');
       return false;
     }
-    const int vs16 = 0xFE0F; // emoji
     if (str.codeUnits.contains(vs16)) {
-      //print('vs16: $this');
       return true;
     }
 
-    // combined characters must be 2 ?
-    if (str.runes.length > 1) {
-      //print('runes > 1: $str');
+    // if number of runes is greater than 1, it's an emoji
+    if (str.codeUnits.length > 1) {
       return true;
     }
 
+    // check in pre-generated list if it's an emoji
     if (emojis1.contains(str)) {
       return true;
     }
