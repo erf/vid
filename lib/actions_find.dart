dbb8ff1bc65e0ca20ad5be579b4fc928d51a8e3c
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-13 20:07:22 +0200

    simplify set cursor in find methods

--- lib/actions_find.dart
+++ lib/actions_find.dart
@@ -8,30 +8,22 @@ typedef FindAction = void Function(FileBuffer, Position, String);
 
 // find the next occurence of the given character on the current line
 void findNextChar(FileBuffer f, Position position, String char) {
-  final p = motionFindNextChar(f, position, char);
-  f.cursor.x = p.x;
-  f.cursor.y = p.y;
+  f.cursor = motionFindNextChar(f, position, char);
   f.mode = Mode.normal;
 }
 
 // find the previous occurence of the given character on the current line
 void findPrevChar(FileBuffer f, Position position, String char) {
-  final p = motionFindPrevChar(f, position, char);
-  f.cursor.x = p.x;
-  f.cursor.y = p.y;
+  f.cursor = motionFindPrevChar(f, position, char);
   f.mode = Mode.normal;
 }
 
 void tillNextChar(FileBuffer f, Position position, String char) {
-  final p = motionTillNextChar(f, position, char);
-  f.cursor.x = p.x;
-  f.cursor.y = p.y;
+  f.cursor = motionTillNextChar(f, position, char);
   f.mode = Mode.normal;
 }
 
 void tillPrevChar(FileBuffer f, Position position, String char) {
-  final p = motionTillPrevChar(f, position, char);
-  f.cursor.x = p.x;
-  f.cursor.y = p.y;
+  f.cursor = motionTillPrevChar(f, position, char);
   f.mode = Mode.normal;
 }
