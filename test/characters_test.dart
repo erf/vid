36ecb94fe6c512e75408305562017e88f43fbf4b
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-15 00:47:33 +0200

    test: CharacterRange methods

--- test/characters_test.dart
+++ test/characters_test.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:characters/characters.dart';
 import 'package:test/test.dart';
 import 'package:vid/characters_ext.dart';
 import 'package:vid/string_ext.dart';
@@ -80,5 +83,27 @@ void main() {
     expect('abc'.ch.replaceCharAt(3, 'd'.ch).string, 'abcd');
     expect('😀😀abc'.ch.replaceCharAt(0, 'd'.ch).string, 'd😀abc');
     expect('😀😀abc'.ch.replaceCharAt(1, 'd'.ch).string, '😀dabc');
+  });
+
+  test('CharacterRange methods', () {
+    final source = 'abc def ghi';
+    final range = CharacterRange(source);
+    expect(range.current, '');
+    range.moveNext(7);
+    expect(range.current, 'abc def');
+    range.collapseToLast('def'.ch);
+    expect(range.current, 'def');
+    range.expandBackAll();
+    expect(range.current, 'abc def');
+    range.dropFirst(4);
+    expect(range.current, 'def');
+    expect(range.source.string, 'abc def ghi');
+    range.dropLast(3);
+    expect(range.current, '');
+    range.moveBack();
+    range.moveBackAll();
+    expect(range.current, 'abc');
+    range.expandAll();
+    expect(range.current, 'abc def ghi');
   });
 }
