d6f949b53cdb9493d000234576fdea5131e60a19
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-16 15:12:30 +0200

    test: getPositionFromIndex test

--- test/file_buffer_ext_test.dart
+++ test/file_buffer_ext_test.dart
@@ -14,4 +14,15 @@ void main() {
     expect(f.getIndexFromPosition(Position(x: 0, y: 1)), 4);
     expect(f.getIndexFromPosition(Position(x: 2, y: 1)), 6);
   });
+
+  test('getPositionFromIndex', () {
+    final f = FileBuffer();
+    f.text = 'abc\ndef';
+    f.createLines();
+
+    expect(f.getPositionFromIndex(0), Position(x: 0, y: 0));
+    expect(f.getPositionFromIndex(2), Position(x: 2, y: 0));
+    expect(f.getPositionFromIndex(4), Position(x: 0, y: 1));
+    expect(f.getPositionFromIndex(6), Position(x: 2, y: 1));
+  });
 }
