309f8ab94f8cafeab1c599c531afe8b69ee8d520
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-16 11:39:08 +0200

    test: rename tests to complementary files

--- test/emoji_test.dart
+++ test/emoji_test.dart
@@ -1,6 +1,7 @@
 import 'package:test/test.dart';
 import 'package:vid/string_ext.dart';
 
+// also see test/string_ext_test.dart
 void main() {
   test('isEmoji test', () {
     expect('a'.isEmoji, false);
