013c0e53c8b5d45b4ff4cc54278e1255e3fbfedd
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-24 12:33:40 +0200

    test: add more pendingActionDelete on objectCurrentLine

--- test/actions_pending_test.dart
+++ test/actions_pending_test.dart
@@ -6,7 +6,18 @@ import 'package:vid/file_buffer_ext.dart';
 import 'package:vid/position.dart';
 
 void main() {
-  test('pendingActionDelete on objectCurrentLine', () {
+  test('pendingActionDelete on objectCurrentLine (first)', () {
+    final f = FileBuffer();
+    f.text = 'abc\ndef\nghi';
+    f.createLines();
+    f.cursor = Position(c: 0, l: 0);
+    final r = objectCurrentLine(f, f.cursor);
+    pendingActionDelete(f, r);
+    expect(f.text, 'def\nghi');
+    expect(f.cursor, Position(c: 0, l: 0));
+  });
+
+  test('pendingActionDelete on objectCurrentLine (middle)', () {
     final f = FileBuffer();
     f.text = 'abc\ndef\nghi';
     f.createLines();
@@ -25,5 +36,25 @@ void main() {
     final r = objectCurrentLine(f, f.cursor);
     pendingActionDelete(f, r);
     expect(f.text, 'abc\ndef');
+  });
+
+  test('pendingActionDelete on objectCurrentLine (one-line)', () {
+    final f = FileBuffer();
+    f.text = 'abc';
+    f.createLines();
+    f.cursor = Position(c: 0, l: 0);
+    final r = objectCurrentLine(f, f.cursor);
+    pendingActionDelete(f, r);
+    expect(f.text, '');
+  });
+
+  test('pendingActionDelete on objectCurrentLine (one-line w newline)', () {
+    final f = FileBuffer();
+    f.text = 'abc\n';
+    f.createLines();
+    f.cursor = Position(c: 0, l: 0);
+    final r = objectCurrentLine(f, f.cursor);
+    pendingActionDelete(f, r);
+    expect(f.text, '');
   });
 }
