fc44b599c96e04f4e6ed80a151c792b1b2ec9ac5
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-24 03:23:39 +0200

    test: add createLines test

--- test/file_buffer_ext_test.dart
+++ test/file_buffer_ext_test.dart
@@ -7,6 +7,25 @@ import 'package:vid/position.dart';
 import 'package:vid/range.dart';
 
 void main() {
+  test('createLines', () {
+    final f = FileBuffer();
+    f.text = 'abc\ndef';
+    f.createLines();
+    expect(f.lines.length, 2);
+    expect(f.lines[0].text.string, 'abc');
+    expect(f.lines[1].text.string, 'def');
+  });
+
+  test('createLines w newline at end', () {
+    final f = FileBuffer();
+    f.text = 'abc\ndef\n';
+    f.createLines();
+    expect(f.lines.length, 3);
+    expect(f.lines[0].text.string, 'abc');
+    expect(f.lines[1].text.string, 'def');
+    expect(f.lines[2].text.string, '');
+  });
+
   test('getPositionFromIndex', () {
     final f = FileBuffer();
     f.text = 'abc\ndef';
