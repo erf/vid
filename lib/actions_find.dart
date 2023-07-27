50c7d985f0e9d6c1f8736653761af59c26cdbcd6
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-27 22:52:42 +0200

    actions_find: use short args

--- lib/actions_find.dart
+++ lib/actions_find.dart
@@ -6,23 +6,23 @@ import 'position.dart';
 typedef FindAction = void Function(FileBuffer, Position, String);
 
 // find the next occurence of the given character on the current line
-void findNextChar(FileBuffer f, Position position, String char) {
-  f.cursor = motionFindNextChar(f, position, char);
+void findNextChar(FileBuffer f, Position p, String c) {
+  f.cursor = motionFindNextChar(f, p, c);
   f.mode = Mode.normal;
 }
 
 // find the previous occurence of the given character on the current line
-void findPrevChar(FileBuffer f, Position position, String char) {
-  f.cursor = motionFindPrevChar(f, position, char);
+void findPrevChar(FileBuffer f, Position p, String c) {
+  f.cursor = motionFindPrevChar(f, p, c);
   f.mode = Mode.normal;
 }
 
-void tillNextChar(FileBuffer f, Position position, String char) {
-  f.cursor = motionTillNextChar(f, position, char);
+void tillNextChar(FileBuffer f, Position p, String c) {
+  f.cursor = motionTillNextChar(f, p, c);
   f.mode = Mode.normal;
 }
 
-void tillPrevChar(FileBuffer f, Position position, String char) {
-  f.cursor = motionTillPrevChar(f, position, char);
+void tillPrevChar(FileBuffer f, Position p, String c) {
+  f.cursor = motionTillPrevChar(f, p, c);
   f.mode = Mode.normal;
 }
