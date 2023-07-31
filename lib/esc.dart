41c51a9da51c6a325195715980bed787762474fa
Erlend Lind Madsen <erlendf80@gmail.com>
2023-07-31 05:00:08 +0200

    esc: reorder

--- lib/esc.dart
+++ lib/esc.dart
@@ -9,20 +9,20 @@ class Esc {
   // escape character
   static const e = '\x1b';
 
+  // home and erase down
+  static const clear = '$e[H$e[J';
+
+  // reset font and background color
+  static const reset = '$e[0m';
+
   // move cursor to x,y
   static curPos({required int l, required int c}) => '$e[$l;${c}H';
 
   // cursor visibility
   static curVis(bool visible) => visible ? '$e[?25h' : '$e[?25l';
 
-  // home and erase down
-  static const clear = '$e[H$e[J';
-
   // set invert
-  static invCol(invert) => invert ? '$e[7m' : '$e[27m';
-
-  // reset font and background color
-  static const reset = '$e[0m';
+  static invCol(bool invert) => invert ? '$e[7m' : '$e[27m';
 
   // alternate buffer
   static altBuf(bool enabled) => enabled ? '$e[?1049h' : '$e[?1049l';
