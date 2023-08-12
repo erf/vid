55dec403075eb2b22b2485722035b93bf7a462d0
Erlend Lind Madsen <erlendf80@gmail.com>
2023-08-12 18:55:56 +0200

    test: use input for pending tests

--- test/actions_pending_test.dart
+++ test/actions_pending_test.dart
@@ -1,40 +1,44 @@
 import 'package:test/test.dart';
 import 'package:vid/actions_operator.dart';
 import 'package:vid/actions_text_objects.dart';
+import 'package:vid/editor.dart';
 import 'package:vid/file_buffer.dart';
 import 'package:vid/file_buffer_lines.dart';
 import 'package:vid/position.dart';
 
 void main() {
   test('pendingActionDelete on objectCurrentLine', () {
-    final f = FileBuffer();
+    final e = Editor();
+    final f = e.file;
     f.text = 'abc\ndef\nghi\n';
     f.createLines();
     f.cursor = Position(c: 0, l: 0);
-    final r = TextObjects.currentLine(f, f.cursor);
-    Operators.delete(f, r);
+    e.input('d', redraw: false);
+    e.input('d', redraw: false);
     expect(f.text, 'def\nghi\n');
     expect(f.cursor, Position(c: 0, l: 0));
   });
 
   test('pendingActionDelete on objectLineUp', () {
-    final f = FileBuffer();
+    final e = Editor();
+    final f = e.file;
     f.text = 'abc\ndef\nghi\n';
     f.createLines();
     f.cursor = Position(c: 1, l: 1);
-    final r = TextObjects.lineUp(f, f.cursor);
-    Operators.delete(f, r);
+    e.input('d', redraw: false);
+    e.input('k', redraw: false);
     expect(f.text, 'ghi\n');
     expect(f.cursor, Position(c: 0, l: 0));
   });
 
   test('pendingActionDelete on objectLineDown', () {
-    final f = FileBuffer();
+    final e = Editor();
+    final f = e.file;
     f.text = 'abc\ndef\nghi\n';
     f.createLines();
     f.cursor = Position(c: 1, l: 0);
-    final r = TextObjects.lineDown(f, f.cursor);
-    Operators.delete(f, r);
+    e.input('d', redraw: false);
+    e.input('j', redraw: false);
     expect(f.text, 'ghi\n');
     expect(f.cursor, Position(c: 0, l: 0));
   });
