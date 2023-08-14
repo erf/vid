c5870aedcf8ea3f0d5fc47eaef174950f45c1500
Erlend Lind Madsen <erlendf80@gmail.com>
2023-08-14 14:04:39 +0200

    word regex now includes empty lines

--- lib/actions_motion.dart
+++ lib/actions_motion.dart
@@ -7,7 +7,7 @@ import 'position.dart';
 import 'utils.dart';
 
 class Motions {
-  static final wordRegex = RegExp(r'(\w+|[^\w\s]+)');
+  static final wordRegex = RegExp(r'(\w+|[^\w\s]+|(?<=\n)\n)');
 
   static Position charNext(FileBuffer f, Position p) {
     return Position(
