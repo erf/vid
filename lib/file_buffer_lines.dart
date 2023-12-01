096116366e0f28b787ca270ab8f6d97c52af1dc5
Erlend Lind Madsen <erlendf80@gmail.com>
2023-12-01 12:08:03 +0700

    Revert "createLines: don't add nl - fixes nl issue w undo"
    This reverts commit 740d2063f338f55f18987559fbc0de219c89a3ad.

--- lib/file_buffer_lines.dart
+++ lib/file_buffer_lines.dart
@@ -5,9 +5,11 @@ import 'line.dart';
 extension FileBufferLines on FileBuffer {
   // split text into lines
   void createLines() {
+    // add missing newline
+    if (!text.endsWith(nl)) text += nl;
+
     // split text into lines (remove last empty line)
-    final splits = text.split(nl);
-    if (text.endsWith('\n')) splits.removeLast();
+    final splits = text.split(nl)..removeLast();
 
     // split text into lines with metadata used for cursor positioning etc.
     lines.clear();
