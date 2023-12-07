1f967f0bd17d476c2672669d10b6fd11fde078a9
Erlend Lind Madsen <erlendf80@gmail.com>
2023-12-07 21:39:51 +0100

    test: rename rList -> l

--- test/range_list_test.dart
+++ test/range_list_test.dart
@@ -4,33 +4,33 @@ import 'package:vid/range_list.dart';
 void main() {
   group('RangeList', () {
     test('should find values inside the ranges', () {
-      var rList = RangeList([IntRange(1, 4), IntRange(5, 8), IntRange(10, 15)]);
-      expect(rList.contains(1), true);
-      expect(rList.contains(3), true);
-      expect(rList.contains(5), true);
-      expect(rList.contains(10), true);
-      expect(rList.contains(15), true);
-      expect(rList.length, 3);
+      final l = RangeList([IntRange(1, 4), IntRange(5, 8), IntRange(10, 15)]);
+      expect(l.contains(1), true);
+      expect(l.contains(3), true);
+      expect(l.contains(5), true);
+      expect(l.contains(10), true);
+      expect(l.contains(15), true);
+      expect(l.length, 3);
     });
 
     test('should not find values outside the ranges', () {
-      var rList = RangeList([IntRange(1, 4), IntRange(5, 8), IntRange(10, 15)]);
-      expect(rList.contains(0), false);
-      expect(rList.contains(9), false);
-      expect(rList.contains(16), false);
+      final l = RangeList([IntRange(1, 4), IntRange(5, 8), IntRange(10, 15)]);
+      expect(l.contains(0), false);
+      expect(l.contains(9), false);
+      expect(l.contains(16), false);
     });
 
     test('should merge overlapping or adjacent ranges', () {
-      var rList =
+      final l =
           RangeList.merged([IntRange(10, 15), IntRange(1, 4), IntRange(4, 8)]);
-      expect(rList.contains(3), true);
-      expect(rList.contains(7), true);
-      expect(rList.length, 2);
+      expect(l.contains(3), true);
+      expect(l.contains(7), true);
+      expect(l.length, 2);
     });
 
     test('should handle an empty list', () {
-      var rList = RangeList([]);
-      expect(rList.contains(1), false);
+      final l = RangeList([]);
+      expect(l.contains(1), false);
     });
   });
 }
