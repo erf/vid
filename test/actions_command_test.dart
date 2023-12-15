8a3dd084eb6e7353381ae9f80b36a12f6b6ad8dc
Erlend Lind Madsen <erlendf80@gmail.com>
2023-12-15 15:09:01 +0100

    test: add search test

--- test/actions_command_test.dart
+++ test/actions_command_test.dart
@@ -1,11 +1,12 @@
 import 'package:test/test.dart';
 import 'package:vid/actions_command.dart';
 import 'package:vid/editor.dart';
+import 'package:vid/file_buffer_lines.dart';
 import 'package:vid/position.dart';
 
 void main() {
   test('substitute should delete the first occurrence of a pattern', () {
-    final e = Editor();
+    final e = Editor(redraw: false);
     final f = e.file;
     f.text = 'hello world';
     f.cursor = Position(l: 0, c: 0);
@@ -14,7 +15,7 @@ void main() {
   });
 
   test('substitute should replace the first occurrence of a pattern', () {
-    final e = Editor();
+    final e = Editor(redraw: false);
     final f = e.file;
     f.text = 'hello world';
     f.cursor = Position(l: 0, c: 0);
@@ -23,11 +24,21 @@ void main() {
   });
 
   test('substitute should replace multiple occurrences of a pattern', () {
-    final e = Editor();
+    final e = Editor(redraw: false);
     final f = e.file;
     f.text = 'this is fun fun fun';
     f.cursor = Position(l: 0, c: 0);
     CommandActions.substitute(e, f, ['s/fun/cool/g']);
     expect(f.text, equals('this is cool cool cool\n'));
+  });
+
+  test('search should find the first occurrence of a pattern', () {
+    final e = Editor(redraw: false);
+    final f = e.file;
+    f.text = 'hello world';
+    f.createLines();
+    f.cursor = Position(l: 0, c: 0);
+    e.input('/world\n');
+    expect(f.cursor, equals(Position(l: 0, c: 6)));
   });
 }
