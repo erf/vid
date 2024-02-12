e0e711a1ca3bdb02fbee0fd4fde16795176b9661
Erlend Lind Madsen <erlendf80@gmail.com>
2024-02-12 03:25:47 +0100

    bench: use Sema.zig in create_lines bench

--- bench/create_lines.dart
+++ bench/create_lines.dart
@@ -5,25 +5,25 @@ import 'package:vid/file_buffer.dart';
 import 'package:vid/file_buffer_lines.dart';
 
 void main() {
-  String text = File('sample-data/eval.c').readAsStringSync();
-  benchmarkCreateLinesWrapModeNone(text);
-  benchmarkCreateLinesWrapModeWord(text);
+  String text = File('sample-data/Sema.zig').readAsStringSync();
+  benchmarkCreateLinesWrapModeNone(text, 80, 24);
+  benchmarkCreateLinesWrapModeWord(text, 80, 24);
 }
 
-void benchmarkCreateLinesWrapModeNone(String text) {
+void benchmarkCreateLinesWrapModeNone(String text, int width, int height) {
   final stopWatch = Stopwatch()..start();
   final f = FileBuffer();
   f.text = text;
-  f.createLines(WrapMode.none, 80, 24);
+  f.createLines(WrapMode.none, width, height);
   print('create lines (wrap none): ${stopWatch.elapsedMilliseconds} ms');
   stopWatch.stop();
 }
 
-void benchmarkCreateLinesWrapModeWord(String text) {
+void benchmarkCreateLinesWrapModeWord(String text, int width, int height) {
   final stopWatch = Stopwatch()..start();
   final f = FileBuffer();
   f.text = text;
-  f.createLines(WrapMode.word, 80, 24);
+  f.createLines(WrapMode.word, width, height);
   print('create lines (wrap word): ${stopWatch.elapsedMilliseconds} ms');
   stopWatch.stop();
 }
