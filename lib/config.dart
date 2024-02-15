8903ff79f9bd5aa98893ffd78d1a83d1532e4053
Erlend Lind Madsen <erlendf80@gmail.com>
2024-02-16 00:11:19 +0100

    default to no wrap mode (because of perf)

--- lib/config.dart
+++ lib/config.dart
@@ -4,6 +4,6 @@ class Config {
   static const int messageTime = 3000;
   static const int tabWidth = 4;
   static const int maxNumUndo = 100;
-  static const WrapMode wrapMode = WrapMode.word;
+  static const WrapMode wrapMode = WrapMode.none;
   static const String breakat = ' !@*-+;:,./?';
 }
