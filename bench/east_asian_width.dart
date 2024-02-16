e2c532fa9cfa2d0dfa210f996ebd982dad8fe2e4
Erlend Lind Madsen <erlendf80@gmail.com>
2024-02-16 03:57:30 +0100

    bench: Regex.isEmoji

--- bench/east_asian_width.dart
+++ bench/east_asian_width.dart
@@ -2,12 +2,14 @@ import 'dart:math';
 
 import 'package:vid/east_asian_width_range_list.dart';
 import 'package:vid/east_asian_width_switch.dart';
+import 'package:vid/regex.dart';
 
 // test the performance of looking up emojis
 void main() {
   final List<String> unicodeChars = generateRandomUnicodeChars(1000000);
   benchmarkEastAsianWidthInRangeList(unicodeChars);
   benchmarkEastAsianWidthSwitch(unicodeChars);
+  benchmarkIsEmojiRegex(unicodeChars);
 }
 
 List<String> generateRandomUnicodeChars(int length) {
@@ -39,5 +41,18 @@ void benchmarkEastAsianWidthSwitch(List<String> unicodeChars) {
   }
   stopwatch.stop();
   print('EastAsianWidthSwitch: ${stopwatch.elapsedMilliseconds}ms');
+  print('contains: $num');
+}
+
+void benchmarkIsEmojiRegex(List<String> unicodeChars) {
+  final stopwatch = Stopwatch()..start();
+  int num = 0;
+  for (final unicodeChar in unicodeChars) {
+    if (Regex.isEmoji.hasMatch(unicodeChar)) {
+      num++;
+    }
+  }
+  stopwatch.stop();
+  print('IsEmojiRegex: ${stopwatch.elapsedMilliseconds}ms');
   print('contains: $num');
 }
