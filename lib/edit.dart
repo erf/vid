f66e516df8b143b2d7748499cb11641883d7c625
Erlend Lind Madsen <erlendf80@gmail.com>
2024-02-18 01:35:22 +0100

    Edit: remove comments

--- lib/edit.dart
+++ lib/edit.dart
@@ -1,27 +1,12 @@
 import 'action_typedefs.dart';
 
 class Edit {
-  // the pending action to be executed
   OperatorFn? operator;
-
-  // the pending motion
   MotionAction? motion;
-
-  // the accumulated text input
   String input = '';
-
-  // the pending operator input
   String opInput = '';
-
-  // the accumulated count input
   String countStr = '';
-
-  // the count of the pending action
   int? count;
-
-  // the find char
   String? findStr;
-
-  // if the pending operator is linewise
   bool linewise = false;
 }
