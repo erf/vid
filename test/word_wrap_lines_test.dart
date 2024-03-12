4488f60edfd8d8d9c8115cf1e0369dcdc50736e5
Erlend Lind Madsen <erlendf80@gmail.com>
2024-03-12 08:00:41 +0100

    test: improve wrap tests

--- test/word_wrap_lines_test.dart
+++ test/word_wrap_lines_test.dart
@@ -4,7 +4,14 @@ import 'package:vid/file_buffer.dart';
 import 'package:vid/file_buffer_lines.dart';
 
 void main() {
-  test('createLines', () {
+  test('no wrap', () {
+    final f = FileBuffer(text: 'hei jeg heter Erlend 😀😀😀');
+    f.createLines(WrapMode.none, 12, 12);
+    expect(f.lines.length, 1);
+    expect(f.lines[0].str, 'hei jeg heter Erlend 😀😀😀 ');
+  });
+
+  test('no wrap two lines', () {
     final f = FileBuffer(text: 'abc\ndef');
     f.createLines(WrapMode.none, 80, 24);
     expect(f.lines.length, 2);
@@ -12,7 +19,14 @@ void main() {
     expect(f.lines[1].str, 'def ');
   });
 
-  test('createLines w newline at end', () {
+  test('no wrap with empty line', () {
+    final f = FileBuffer(text: '\n');
+    f.createLines(WrapMode.none, 80, 24);
+    expect(f.lines.length, 1);
+    expect(f.lines[0].str, ' ');
+  });
+
+  test('no wrap with newline at end of file', () {
     final f = FileBuffer(text: 'abc\ndef\n');
     f.createLines(WrapMode.none, 80, 24);
     expect(f.lines.length, 2);
@@ -20,7 +34,7 @@ void main() {
     expect(f.lines[1].str, 'def ');
   });
 
-  test('createLines with wordwrap', () {
+  test('word wrap simple', () {
     final f = FileBuffer(text: 'abc def ghi jkl');
     f.createLines(WrapMode.word, 8, 10);
     expect(f.lines.length, 2);
@@ -28,7 +42,7 @@ void main() {
     expect(f.lines[1].str, 'ghi jkl ');
   });
 
-  test('createLines with emoji at end', () {
+  test('word wrap with emoji at end', () {
     final f = FileBuffer(text: 'abc def😀 ghi jkl\n');
     f.createLines(WrapMode.word, 8, 20);
     expect(f.lines.length, 3);
@@ -37,7 +51,7 @@ void main() {
     expect(f.lines[2].str, 'ghi jkl ');
   });
 
-  test('createLines word wrapped use last breakat point', () {
+  test('word wrap check indices', () {
     final f = FileBuffer(text: 'The old bookstore exuded');
     f.createLines(WrapMode.word, 20, 20);
     expect(f.lines.length, 2);
@@ -49,14 +63,7 @@ void main() {
     expect(f.lines[1].end, 25);
   });
 
-  test('no wrap', () {
-    final f = FileBuffer(text: 'hei jeg heter Erlend 😀😀😀');
-    f.createLines(WrapMode.none, 12, 12);
-    expect(f.lines.length, 1);
-    expect(f.lines[0].str, 'hei jeg heter Erlend 😀😀😀 ');
-  });
-
-  test('word wrap', () {
+  test('word wrap to four lines', () {
     final f = FileBuffer(text: 'hei jeg heter Erlend 😀😀😀');
     f.createLines(WrapMode.word, 12, 12);
     expect(f.lines.length, 4);
@@ -66,7 +73,7 @@ void main() {
     expect(f.lines[3].str, '😀😀😀 ');
   });
 
-  test('char wrap', () {
+  test('char wrap to three lines', () {
     final f = FileBuffer(text: 'hei jeg heter Erlend 😀😀😀');
     f.createLines(WrapMode.char, 12, 12);
     expect(f.lines.length, 3);
