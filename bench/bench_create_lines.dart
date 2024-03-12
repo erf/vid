56bfda9f2934a2fbd839930d8e7956897b771cdb
Erlend Lind Madsen <erlendf80@gmail.com>
2024-03-12 02:08:35 +0100

    bench: benchmarkCreateLinesWrapModeChar

--- bench/bench_create_lines.dart
+++ bench/bench_create_lines.dart
@@ -7,6 +7,7 @@ import 'package:vid/file_buffer_lines.dart';
 void main() {
   String text = File('sample-data/Sema.zig').readAsStringSync();
   benchmarkCreateLinesWrapModeNone(text, 80, 24);
+  benchmarkCreateLinesWrapModeChar(text, 80, 24);
   benchmarkCreateLinesWrapModeWord(text, 80, 24);
 }
 
@@ -15,6 +16,14 @@ void benchmarkCreateLinesWrapModeNone(String text, int width, int height) {
   final f = FileBuffer(text: text);
   f.createLines(WrapMode.none, width, height);
   print('create lines (wrap none): ${stopWatch.elapsedMilliseconds} ms');
+  stopWatch.stop();
+}
+
+void benchmarkCreateLinesWrapModeChar(String text, int width, int height) {
+  final stopWatch = Stopwatch()..start();
+  final f = FileBuffer(text: text);
+  f.createLines(WrapMode.char, width, height);
+  print('create lines (wrap char): ${stopWatch.elapsedMilliseconds} ms');
   stopWatch.stop();
 }
 
