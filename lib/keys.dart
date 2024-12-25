7a2e65ec19d86936510c303ce0f17cb1e89acd56
Erlend Lind Madsen <erlendf80@gmail.com>
2024-12-25 19:57:46 +0100

    keys: use shorthand

--- lib/keys.dart
+++ lib/keys.dart
@@ -1,11 +1,11 @@
 class Keys {
   static const String newline = '\n'; // use Platform.lineTerminator ?
-  static const String escape = '\u001b';
-  static const String backspace = '\u007f';
-  static const String bell = '\u0007';
-  static const String ctrlD = '\u0004';
-  static const String ctrlU = '\u0015';
-  static const String ctrlX = '\u0018';
-  static const String ctrlA = '\u0001';
-  static const String ctrlW = '\u0017';
+  static const String escape = '\x1b';
+  static const String backspace = '\x7f';
+  static const String bell = '\x07';
+  static const String ctrlD = '\x04';
+  static const String ctrlU = '\x15';
+  static const String ctrlX = '\x18';
+  static const String ctrlA = '\x01';
+  static const String ctrlW = '\x17';
 }
