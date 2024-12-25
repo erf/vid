86306f7fcc9de24be74a20d3f9aec0ff3508b14c
Erlend Lind Madsen <erlendf80@gmail.com>
2024-12-25 22:14:05 +0100

    test: dont add additional newline (skipped for now)

---
+++ test/file_buffer_lines_test.dart
@@ -0,0 +1,19 @@
+import 'package:test/test.dart';
+import 'package:vid/config.dart';
+import 'package:vid/editor.dart';
+import 'package:vid/file_buffer_lines.dart';
+import 'package:vid/position.dart';
+import 'package:vid/terminal.dart';
+
+void main() {
+  test('don\'t delete newline at end of file (and create extra newline)', () {
+    final e = Editor(terminal: TestTerminal(80, 24), redraw: false);
+    final f = e.file;
+    f.text = 'abc\n';
+    f.createLines(e, WrapMode.none);
+    f.cursor = Position(c: 3, l: 0);
+    e.input('xu');
+    expect(f.text, 'abc\n');
+    expect(f.cursor, Position(c: 3, l: 0));
+  }, skip: true);
+}
