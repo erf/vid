0c78f0a45a461dd07efbe7b947fe369cf056264c
Erlend Lind Madsen <erlendf80@gmail.com>
2024-12-30 13:50:59 +0100

    test: remove comma

--- test/file_buffer_text_test.dart
+++ test/file_buffer_text_test.dart
@@ -146,19 +146,16 @@ void main() {
     expect(f.text, '123\n');
   });
 
-  test(
-    'delete last newline then undo should not create extra newline',
-    () {
-      final e = Editor(terminal: TestTerminal(80, 24), redraw: false);
-      final f = e.file;
-      f.text = 'a\n';
-      f.createLines(e, WrapMode.none);
-      f.cursor = Position(c: 1, l: 0);
-      e.input('xu');
-      expect(f.text, 'a\n');
-      expect(f.cursor, Position(c: 1, l: 0));
-    },
-  );
+  test('delete last newline undo should not create extra newline', () {
+    final e = Editor(terminal: TestTerminal(80, 24), redraw: false);
+    final f = e.file;
+    f.text = 'a\n';
+    f.createLines(e, WrapMode.none);
+    f.cursor = Position(c: 1, l: 0);
+    e.input('xu');
+    expect(f.text, 'a\n');
+    expect(f.cursor, Position(c: 1, l: 0));
+  });
 
   test('delete newline at end of file', () {
     final e = Editor(terminal: TestTerminal(80, 24), redraw: false);
