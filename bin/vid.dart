3ac85211244e11c4c639ce625709d9e26f28930d
Erlend Lind Madsen <erlendf80@gmail.com>
2025-01-22 00:36:10 +0700

    vid: fix use TerminalImpl

--- bin/vid.dart
+++ bin/vid.dart
@@ -1,6 +1,6 @@
 import 'package:vid/editor.dart' as vid;
-import 'package:vid/terminal_realdart';
+import 'package:vid/terminal_impl.dart';
 
 void main(List<String> args) {
-  vid.Editor(terminal: TerminalReal()).init(args);
+  vid.Editor(terminal: TerminalImpl()).init(args);
 }
