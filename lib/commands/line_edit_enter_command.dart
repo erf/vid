584156eb7640a743d7a9eb7a27da006a00ee0062
Erlend Lind Madsen <erlendf80@gmail.com>
2025-01-26 00:57:46 +0700

    move commands inside LineEditEnterCommand

--- lib/commands/line_edit_enter_command.dart
+++ lib/commands/line_edit_enter_command.dart
@@ -7,36 +7,36 @@ import '../modes.dart';
 import '../regex.dart';
 import 'command.dart';
 
-const lineEditCommands = <String, Function>{
-  '': LineEdit.noop,
-  'q': LineEdit.quit,
-  'quit': LineEdit.quit,
-  'q!': LineEdit.forceQuit,
-  'quit!': LineEdit.forceQuit,
-  'o': LineEdit.open,
-  'open': LineEdit.open,
-  'r': LineEdit.read,
-  'read': LineEdit.read,
-  'w': LineEdit.write,
-  'write': LineEdit.write,
-  'wq': LineEdit.writeAndQuit,
-  'x': LineEdit.writeAndQuit,
-  'exit': LineEdit.writeAndQuit,
-  'nowrap': LineEdit.setNoWrap,
-  'charwrap': LineEdit.setCharWrap,
-  'wordwrap': LineEdit.setWordWrap,
-};
-
 class LineEditEnterCommand extends Command {
   const LineEditEnterCommand();
+
+  static const commands = <String, Function>{
+    '': LineEdit.noop,
+    'q': LineEdit.quit,
+    'quit': LineEdit.quit,
+    'q!': LineEdit.forceQuit,
+    'quit!': LineEdit.forceQuit,
+    'o': LineEdit.open,
+    'open': LineEdit.open,
+    'r': LineEdit.read,
+    'read': LineEdit.read,
+    'w': LineEdit.write,
+    'write': LineEdit.write,
+    'wq': LineEdit.writeAndQuit,
+    'x': LineEdit.writeAndQuit,
+    'exit': LineEdit.writeAndQuit,
+    'nowrap': LineEdit.setNoWrap,
+    'charwrap': LineEdit.setCharWrap,
+    'wordwrap': LineEdit.setWordWrap,
+  };
 
   @override
   void execute(Editor e, FileBuffer f, String s) {
     final String command = f.edit.lineEdit;
     List<String> args = command.split(' ');
     String cmd = args.isNotEmpty ? args.first : command;
-    if (lineEditCommands.containsKey(cmd)) {
-      lineEditCommands[cmd]!(e, f, args);
+    if (commands.containsKey(cmd)) {
+      commands[cmd]!(e, f, args);
       return;
     }
     if (command.startsWith(Regex.substitute)) {
