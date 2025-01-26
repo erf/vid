3be66764882ba49d8deaac97296177a01691dd2a
Erlend Lind Madsen <erlendf80@gmail.com>
2025-01-26 13:08:40 +0700

    test: add same operator test with count (failing)

--- test/actions_operator_test.dart
+++ test/actions_operator_test.dart
@@ -134,4 +134,13 @@ void main() {
     e.input('gUe');
     expect(f.text, 'ABC\n');
   });
+
+  test('count should work for dd', () {
+    final e = Editor(terminal: TerminalDummy(80, 24), redraw: false);
+    final f = e.file;
+    f.text = 'abc\ndef\nghi\n';
+    f.createLines(e, WrapMode.none);
+    e.input('2dd');
+    expect(f.text, 'ghi\n');
+  });
 }
