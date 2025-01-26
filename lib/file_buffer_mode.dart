f574c70c12389f6e5a33f7f416a310f73b0027c4
Erlend Lind Madsen <erlendf80@gmail.com>
2025-01-26 23:38:50 +0700

    file_buffer_mode: simplify

--- lib/file_buffer_mode.dart
+++ lib/file_buffer_mode.dart
@@ -7,24 +7,10 @@ extension FileBufferMode on FileBuffer {
   void setMode(Editor e, Mode mode) {
     switch (mode) {
       case Mode.normal:
-        if (this.mode == Mode.insert ||
-            this.mode == Mode.command ||
-            this.mode == Mode.search) {
-          e.terminal.write(Esc.cursorStyleBlock);
-        }
-        break;
-      case Mode.operatorPending:
-        break;
+        e.terminal.write(Esc.cursorStyleBlock);
       case Mode.insert:
-        if (this.mode != Mode.insert) {
-          e.terminal.write(Esc.cursorStyleLine);
-        }
-        break;
-      case Mode.replace:
-        break;
-      case Mode.command:
-        break;
-      case Mode.search:
+        e.terminal.write(Esc.cursorStyleLine);
+      default:
         break;
     }
     this.mode = mode;
