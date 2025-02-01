5812878f309cded8d157d2d5e8f998d91868e4d2
Erlend Lind Madsen <erlendf80@gmail.com>
2025-02-02 03:57:57 +0700

    operator_pending_same_command: dont need to set cursor

--- lib/commands/operator_pending_same_command.dart
+++ lib/commands/operator_pending_same_command.dart
@@ -5,7 +5,6 @@ import '../editor.dart';
 import '../file_buffer.dart';
 import '../file_buffer_mode.dart';
 import '../modes.dart';
-import '../motions/line_start_motion.dart';
 import 'command.dart';
 
 class OperatorPendingSameCommand extends Command {
@@ -18,7 +17,6 @@ class OperatorPendingSameCommand extends Command {
     if (f.edit.op == func) {
       f.edit.motion = LinewiseMotion();
       e.commitEdit(f.edit);
-      f.cursor = LineStartMotion().run(f, f.cursor);
     } else {
       f.setMode(e, Mode.normal);
       f.edit = Edit();
