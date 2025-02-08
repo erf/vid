ae07ff330c2447bf925cc642ba581525d68f3fa3
Erlend Lind Madsen <erlendf80@gmail.com>
2025-02-09 00:14:43 +0700

    simplify linewise_motion to use lines instead of regex

--- lib/motions/linewise_motion.dart
+++ lib/motions/linewise_motion.dart
@@ -1,8 +1,6 @@
-import 'package:vid/file_buffer_text.dart';
 import 'package:vid/motions/motion.dart';
 
 import '../file_buffer.dart';
-import '../keys.dart';
 import '../position.dart';
 
 class LinewiseMotion extends Motion {
@@ -10,11 +8,9 @@ class LinewiseMotion extends Motion {
 
   @override
   Position run(FileBuffer f, Position p, {bool op = true}) {
-    final start = f.byteIndexFromPosition(p);
-    final matches = RegExp(Keys.newline).allMatches(f.text, start);
-    if (matches.isEmpty) return p;
-    final m = matches.first;
-    final next = f.positionFromByteIndex(m.start);
-    return Position(l: next.l, c: next.c + 1);
+    final line = f.lines[p.l];
+    if (p.l == f.lines.length - 1) return Position(l: p.l, c: line.charLen);
+    if (p.c == line.charLen) return Position(l: p.l + 1, c: line.charLen);
+    return Position(l: p.l, c: line.charLen);
   }
 }
