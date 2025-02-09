e7da4dd1a059da6e0d4a9909985320cf32cf8a37
Erlend Lind Madsen <erlendf80@gmail.com>
2025-02-09 09:24:43 +0700

    simplify find_prev_char_motion to use Motions.regexPrev

--- lib/motions/find_prev_char_motion.dart
+++ lib/motions/find_prev_char_motion.dart
@@ -1,6 +1,6 @@
 import 'package:vid/file_buffer_io.dart';
-import 'package:vid/file_buffer_text.dart';
 
+import '../actions/motions.dart';
 import '../file_buffer.dart';
 import '../position.dart';
 import 'motion.dart';
@@ -13,10 +13,6 @@ class FindPrevCharMotion extends Motion {
   @override
   Position run(FileBuffer f, Position p, {bool op = false}) {
     f.edit.findStr = c ?? f.edit.findStr ?? f.readNextChar();
-    final int start = f.byteIndexFromPosition(p);
-    final matches = f.edit.findStr!.allMatches(f.text.substring(0, start));
-    if (matches.isEmpty) return p;
-    final Match match = matches.last;
-    return f.positionFromByteIndex(match.start);
+    return Motions.regexPrev(f, p, RegExp(f.edit.findStr!));
   }
 }
