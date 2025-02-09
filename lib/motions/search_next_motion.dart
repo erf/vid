d4f95a8096c29a8b3792d3c18bf0f0f6cc7c2fda
Erlend Lind Madsen <erlendf80@gmail.com>
2025-02-09 09:36:40 +0700

    simplify search_next_motion to use Motions.regexNext

--- lib/motions/search_next_motion.dart
+++ lib/motions/search_next_motion.dart
@@ -1,5 +1,4 @@
-import 'package:vid/file_buffer_text.dart';
-
+import '../actions/motions.dart';
 import '../file_buffer.dart';
 import '../position.dart';
 import 'motion.dart';
@@ -10,11 +9,6 @@ class SearchNextMotion extends Motion {
   @override
   Position run(FileBuffer f, Position p, {bool op = false}) {
     final String pattern = f.edit.findStr ?? '';
-    final int start = f.byteIndexFromPosition(p);
-    final RegExpMatch? match = RegExp(RegExp.escape(pattern))
-        .allMatches(f.text, start + 1)
-        .firstOrNull;
-    if (match == null) return p;
-    return f.positionFromByteIndex(match.start);
+    return Motions.regexNext(f, p, RegExp(RegExp.escape(pattern)), skip: 1);
   }
 }
