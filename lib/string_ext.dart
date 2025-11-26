0f45c18b53224c853baa68d1cf44466c7503e958
Erlend Lind Madsen <erlendf80@gmail.com>
2025-11-26 10:41:54 +0700

    string_ext: simplify tabsToSpaces by using string op mult

--- lib/string_ext.dart
+++ lib/string_ext.dart
@@ -7,12 +7,8 @@ extension StringExt on String {
   // Shorthand for characters (Characters(this))
   Characters get ch => characters;
 
-  // generate a string of spaces the same length as the tab width
-  String tabSpaces(int tabWidth) => List.generate(tabWidth, (_) => ' ').join();
-
   // replace all tabs with spaces
-  String tabsToSpaces(int tabWidth) =>
-      replaceAll(Keys.tab, tabSpaces(tabWidth));
+  String tabsToSpaces(int tabWidth) => replaceAll(Keys.tab, ' ' * tabWidth);
 
   // Try to determine the rendered width of a single character
   int charWidth(int tabWidth) => Unicode.charWidth(this, tabWidth: tabWidth);
