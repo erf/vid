35e8927f803307f5d4af4b59f6962f0dcf1affd3
Erlend Lind Madsen <erlendf80@gmail.com>
2026-01-07 23:10:46 +0100

    remove unused scroll event regex

--- lib/regex.dart
+++ lib/regex.dart
@@ -2,7 +2,6 @@ class Regex {
   static final word = RegExp(r'([\wæøå]+|[^\w\s]+|(?<=\n)\n)');
   static final wordCap = RegExp(r'(\S+|(?<=\n)\n)');
   static final number = RegExp(r'((?:-)?\d+)');
-  static final scrollEvents = RegExp('\x1b[O[][A-D]');
   static final nonSpace = RegExp(r'\S');
   static final substitute = RegExp(r's/.+[/]?.*');
 
